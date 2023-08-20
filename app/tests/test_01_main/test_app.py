6c8649f654a83956237b2852eaa14b0541c903f5
Lenclove <32355298+Lenclove@users.noreply.github.com>
2023-08-20 18:15:54 +0800

    upgrade dependecies

--- app/tests/test_01_main/test_app.py
+++ app/tests/test_01_main/test_app.py
@@ -1,6 +1,7 @@
 import pytest
 
 from app.main import app
+
 # import pytest
 from httpx import AsyncClient
 from fastapi.testclient import TestClient
@@ -8,7 +9,7 @@ from fastapi.testclient import TestClient
 
 @pytest.mark.asyncio
 async def test_get_index():
-    async with AsyncClient(app=app, base_url='http://test') as ac:
-        response = await ac.get('/')
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
     assert response.status_code == 200
-    assert response.text == 'Hello World!'
+    assert response.text == "Hello World!"
